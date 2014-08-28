bca49168e26c6a7f9761ead4053dbe3d4fdbc1e9
Chadwick Flores <chadwickflores@gmail.com>
2014-08-28 13:40:33 -0500

    add book genres

--- js/app.js
+++ js/app.js
@@ -11,8 +11,19 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+App.BooksController = Ember.ArrayController.extend({
+	sortProperties: ['title']
+});
+
 App.ApplicationAdapter = DS.FixtureAdapter.extend({
 
+});
+
+App.BookDetailsComponent = Ember.Component.extend({
+	classNameBindings: ['ratingClass'],
+	ratingClass: function() {
+		return "rating-" + this.get('book.rating');
+	}.property('book.rating')
 });
 
 App.Book = DS.Model.extend({
@@ -21,6 +32,7 @@ App.Book = DS.Model.extend({
 	review: DS.attr(),
 	rating: DS.attr('number'),
 	amazon_id: DS.attr(),
+	genre: DS.belongsTo('genre'),
 	url: function() {
 		return "http://www.amazon.com/gp/product/"+this.get('amazon_id')+"/adamfortuna-20";
 	}.property('amazon_id'),
@@ -36,7 +48,8 @@ App.Book.FIXTURES = [
 	author: 'Seymour A. Papert',
 	review: 'Although this book foucuses ont he cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themselves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.',
 	rating: 5,
-	amazon_id: '0465046746'
+	amazon_id: '0465046746',
+	genre: 3
 },
 {
 	id: 2,
@@ -44,7 +57,8 @@ App.Book.FIXTURES = [
 	author: 'Dan Simmons',
 	review: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude to the larger storyline. The scope of the story is ambitious - spanning time, planets religion and love.",
 	rating: 5,
-	amazon_id: '0553283685'
+	amazon_id: '0553283685',
+	genre: 1
 },
 {
 	id: 3,
@@ -52,6 +66,29 @@ App.Book.FIXTURES = [
 	author: 'Leander Kahney',
 	review: "Even though I respect Ive, I felt this biography only hit skin deep. It went over all the major events in his life, his passion for design, awards he achieved -- but that's really it. I don't feel I know him anymore than before reading this.",
 	rating: 2,
-	amazon_id: '159184617X'
+	amazon_id: '159184617X',
+	genre: 3
 }
+];
+
+App.Genre = DS.Model.extend({
+	name: DS.attr(),
+	books: DS.hasMany('book'),
+});
+
+App.Genre.FIXTURES = [
+	{
+		id: 1,
+		name: 'Science Fiction',
+		books: [2]
+	},
+	{
+		id: 2,
+		name: 'Fiction',
+	},
+	{
+		id: 3,
+		name: 'Non-Fiction',
+		books: [1,3]
+	}
 ];
